c9b4a864c293122c1f91e65f1c24e6a819c0a3cb
Henry Nissen <henrynissen@hotmail.com>
2021-10-21 14:41:21 -0400

    industry standard btw

--- ChampionFightingGame/utils.h
+++ ChampionFightingGame/utils.h
@@ -1,8 +1,8 @@
+#pragma once
 using namespace std;
 #include <iostream>
 #include "SDL.h"
 #include <sstream>
-#pragma once
 typedef char i8;
 typedef unsigned char u8;
 typedef short i16;
@@ -114,8 +114,7 @@ enum
 	CHARA_KIND_MAX,
 };
 
-enum
-{
+enum {
 	CHARA_STATUS_WAIT,
 	CHARA_STATUS_WALKF,
 	CHARA_STATUS_WALKB,
@@ -161,9 +160,19 @@ enum
 
 	CHARA_ERIC_STATUS_FIREBALL = CHARA_STATUS_START_SPECIFIC,
 	CHARA_ERIC_STATUS_MAX,
+};
 
 
-	CHARA_STATUS_MAX = std::max((int)CHARA_ROY_STATUS_MAX, (int)CHARA_ERIC_STATUS_MAX),
+//For this to compile, max_status needs to be defined SPECIFICALLY after all of the CHARA_X_STATUS_MAX constants are set, but before CHARA_STATUS_MAX is set.
+
+static constexpr int max_status() {
+	return max(CHARA_ROY_STATUS_MAX, CHARA_ERIC_STATUS_MAX);
+}
+
+//Yes, I did try declaring max_status and defining it in utils.cpp. No, it did not work.
+
+enum {
+	CHARA_STATUS_MAX = max_status(), //thanks cpp
 };
 
 

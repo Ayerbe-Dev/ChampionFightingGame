0c163bee1ce4b03997a0b27da26d127d10e994e7
Henry Nissen <63805100+Ayerbe-Dev@users.noreply.github.com>
2022-06-12 10:18:03 -0400

    geometry shader but now it works

--- ChampionFightingGame/resource/shaders/geometry_main.glsl
+++ ChampionFightingGame/resource/shaders/geometry_main.glsl
@@ -1,6 +1,6 @@
 #version 330 core
 layout (triangles) in;
-layout (triangle_strip, max_vertices = 6) out;
+layout (triangle_strip, max_vertices = 3) out;
 
 in VS_OUT {
 
@@ -18,11 +18,13 @@ out GS_OUT {
 } gs_out;
 
 void main() {
-    gl_Position = gl_in[0].gl_Position; 
-    gs_out.FragPos = gs_in[0].FragPos;
-    gs_out.Normal = gs_in[0].Normal;
-    gs_out.TexCoords = gs_in[0].TexCoords;
-    gs_out.FragPosLightSpace = gs_in[0].FragPosLightSpace;
-    EmitVertex();
+    for (int i = 0, max = gl_in.length(); i < max; i++) {
+        gl_Position = gl_in[i].gl_Position; 
+        gs_out.FragPos = gs_in[i].FragPos;
+        gs_out.Normal = gs_in[i].Normal;
+        gs_out.TexCoords = gs_in[i].TexCoords;
+        gs_out.FragPosLightSpace = gs_in[i].FragPosLightSpace;
+        EmitVertex();
+    }
     EndPrimitive();
 }  

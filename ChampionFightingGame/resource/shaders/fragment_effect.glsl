b542051f3b7a8da4287beb8f23e70244f6ded61c
Henry Nissen <63805100+Ayerbe-Dev@users.noreply.github.com>
2024-08-31 18:11:47 -0400

    oh ok particle effects work again

--- ChampionFightingGame/resource/shaders/fragment_effect.glsl
+++ ChampionFightingGame/resource/shaders/fragment_effect.glsl
@@ -2,6 +2,7 @@
 layout (location = 0) out vec4 g_diffuse;
 layout (location = 1) out vec4 g_pos_outline;
 layout (location = 2) out vec4 g_normal_spec;
+layout (location = 3) out vec4 g_emission;
 
 in GS_OUT {
     vec2 TexCoords;
@@ -12,6 +13,10 @@ uniform vec4 f_colormod;
 
 void main() {
     g_diffuse = texture(f_texture, fs_in.TexCoords);
+    if (g_diffuse.a < 0.2) {
+        discard;
+    }
     g_diffuse.rgb += f_colormod.rgb;
     g_diffuse.a -= f_colormod.a;
+    g_emission = g_diffuse;
 }
